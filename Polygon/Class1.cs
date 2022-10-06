93587b19f1aa76eff5411955ecb91c61b2e96183
Ivan Laptev <73838226+LaptevIvan@users.noreply.github.com>
2022-10-06 15:50:08 +0300

    commit

---
+++ Polygon/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Polygon
+{
+    abstract class Shape {
+        protected static int R;
+        protected static Color C;
+        protected int x, y;
+
+        static Shape() {
+            C = Color.Blue;
+            R = 10;
+        }
+
+        public Shape(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public abstract void Draw(Graphics G);
+
+        public abstract bool IsInside(int x, int y);
+    }
+
+    class Circle : Shape {
+        public Circle(int x, int y) : base(x, y) { }
+        
+        public override void Draw(Graphics G) {
+            Brush brush = new SolidBrush(C);
+            G.FillEllipse(brush, x - R, y + R, x + 2 * R, y + 2 * R);
+        }
+
+        public override bool IsInside(int x, int y) { 
+            return Math.Sqrt((this.x - x) * (this.x - x)) + Math.Sqrt((this.y - y) * this.y - y) < R;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public Triangle(int x, int y) : base(x, y) { }
+
+        public override void Draw(Graphics G) {
+            Brush brush = new SolidBrush(C);
+        }
+
+        public override bool IsInside(int x, int y) { return false; } // TODO: т. Герона
+    }
+
+    class Square : Shape
+    {
+        public Square(int x, int y) : base(x, y) { }
+
+        public override void Draw(Graphics G) {
+
+        }
+
+        public override bool IsInside(int x, int y)
+        {
+            return false;
+        }
+    }
+}
---
+++ Polygon/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polygon
+{
+    public partial class Form1 : Form
+    {
+        List<Shape> shapes = new List<Shape> { };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e) {
+            Circle circle = new Circle(e.X, e.Y);
+            shapes.Add(circle);
+       
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            shapes[0].Draw(e.Graphics);
+        }
+    }
+}
